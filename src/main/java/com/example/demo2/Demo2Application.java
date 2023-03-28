4e8b356a1696f3a78157a8001be9def7242cd7ce
kxr <kongxr1990@163.com>
2023-03-28 23:32:40 +0800

    HAH

--- src/main/java/com/example/demo2/Demo2Application.java
+++ src/main/java/com/example/demo2/Demo2Application.java
@@ -5,6 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Demo2Application {
+	/**
+	 *
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		SpringApplication.run(Demo2Application.class, args);
